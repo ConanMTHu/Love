01a9a74988a9752b73f0e153d76c7e7afdac21b4
Tommy <tommyyaphetshu@163.com>
2017-04-14 18:13:56 +0800

    统一 vc 跳转 ….1

--- Invoker/Extension/UIKit/UIViewController+Tommy.swift
+++ Invoker/Extension/UIKit/UIViewController+Tommy.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
+public enum ViewControllerJumpTargetStyle {
+    case push
+    case pop
+    case present
+    case dismiss
+    
+}
+
 // MARK: - 自身业务
-extension UIViewController {
-//    public func open(to vc: UIViewController.Type) {
-//        let storyboardID = String(describing: vc)
-//        let viewController = UIStoryboard(name: "Root", bundle: nil).instantiateViewController(withIdentifier: storyboardID)
-//        
-//    }
+public extension UIViewController {
+    public func open(action: ViewControllerJumpTargetStyle = .push, vc: UIViewController.Type, param: [String: Any]? = nil) {
+        let storyboardID = String(describing: vc)
+        let viewController = UIStoryboard(name: "Root", bundle: nil).instantiateViewController(withIdentifier: storyboardID)
+        viewController.param = param ?? [:]
+    }
+}
+
+// MARK: - vc 之间的跳转传值通过一个 Dic 来实现
+public extension UIViewController {
+    private struct RuntimeKey {
+        static let paramKey = UnsafeRawPointer.init(bitPattern: "paramKey".hashValue)
+        /// ...其他Key声明
+    }
+    
+    var param: [String: Any] {
+        set {
+            objc_setAssociatedObject(self, UIViewController.RuntimeKey.paramKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+        
+        get {
+            return  objc_getAssociatedObject(self, UIViewController.RuntimeKey.paramKey) as! [String : Any]
+        }
+    }
 }
 
 // MARK: - flow
