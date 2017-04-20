d2556923f33a2f2d0f9a7fb9bbd0af7000d083b2
Tommy <tommyyaphetshu@163.com>
2017-04-20 11:57:07 +0800

    饼状图+引线+数据 over

--- Invoker/Views/Chart/PieChartView.swift
+++ Invoker/Views/Chart/PieChartView.swift
@@ -11,12 +11,13 @@ import UIKit
 class PieChartView: UIView {
     // 半径
     let radius: CGFloat = 80.0
-    let datas: [CGFloat] = [0.4,0.3,0.2,0.1]
-    let colors: [UIColor] = [.orange,.red,.blue,.green]
+    let datas: [CGFloat] = [6100, 3400, 5000, 10000, 4000, 8000]
+    let colors: [UIColor] = [.orange, .red, .blue, .purple, .gray, .brown]
+    let textList: [NSString] = ["住房", "吃喝", "交通", "旅行", "医疗", "爱人"]
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .black
+        self.backgroundColor = UIColor(hexString: "#efefef")
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -29,9 +30,87 @@ class PieChartView: UIView {
         // 对应小圆点的中心点
         let smallCircleCenterX = center.x + (radius + 10) * cos(centerAngle)
         let smallCircleCenterY = center.y + (radius + 10) * sin(centerAngle)
+        // 线的折点+终点
+        var lineLosePointX: CGFloat = 0.0, lineLosePointY: CGFloat = 0.0, lineEndPointX: CGFloat = 0.0, lineEndPointY: CGFloat = 0.0
+        // 数字的起点
+        var numberStartPointX: CGFloat = 0.0, numberStartPointY: CGFloat = 0.0
+        // 文本的起点
+        var textStartPointX: CGFloat = 0.0, textStartPointY: CGFloat = 0.0
         
-        // 折线的折点
-        let brokenLineX =
+        lineLosePointX = smallCircleCenterX + 10.0*cos(centerAngle)
+        lineLosePointY = smallCircleCenterY + 10.0*sin(centerAngle)
+        
+        // 数字:数值+比例
+        let sum = datas.reduce(0,{$0 + $1})
+        let numberData = String.init(format: "%.1f(%.1f%%)",datas[index],CGFloat(datas[index])/sum*100) as NSString
+        let numberSize = numberData.size(attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 10.0)])
+        let textSize = textList[index].size(attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 12.0)])
+        // 数字和文本的宽度+高度
+        var width: CGFloat = 0.0, height: CGFloat = 40.0
+        var maxWidth = numberSize.width > textSize.width ? numberSize.width : textSize.width
+        if maxWidth < radius {
+            maxWidth = radius
+            width = radius
+        } else {
+            if (maxWidth + 10 > frame.size.width/2-5) {
+                maxWidth = frame.size.width/2-5;
+            }
+            width = maxWidth + 10.0;
+        }
+        
+        // 中心点的右边
+        if smallCircleCenterX > frame.size.width / 2 {            //指引线的终点
+            lineEndPointX = lineLosePointX + width; //
+            lineEndPointY = lineLosePointY; //
+            // 数字
+            numberStartPointX = lineEndPointX - numberSize.width;
+            numberStartPointY = lineEndPointY - numberSize.height;
+            // 文本
+            textStartPointX = lineEndPointX - width;
+            textStartPointY = lineEndPointY;
+        } else {
+            // 中心点的右边
+            // 指引线的终点
+            lineEndPointX = lineLosePointX - width;
+            if (lineEndPointX < 3) {
+                lineEndPointX = 3;
+            }
+            lineEndPointY = lineLosePointY;
+            // 数字
+            numberStartPointX = lineEndPointX ;
+            numberStartPointY = lineEndPointY - numberSize.height;
+            // 文本
+            textStartPointX = lineEndPointX;
+            textStartPointY = lineEndPointY;
+        }
+        
+        // 开始画小圆点
+        let smallCirclePath = UIBezierPath(arcCenter: CGPoint(x: smallCircleCenterX, y: smallCircleCenterY), radius: 4, startAngle: 0.0, endAngle: CGFloat.degreesToRadians(360.0), clockwise: true)
+        color.set()
+        smallCirclePath.fill()
+        smallCirclePath.stroke()
+        
+        // 画折线
+        context.beginPath()
+        context.move(to: CGPoint(x: smallCircleCenterX, y: smallCircleCenterY))
+        context.addLine(to: CGPoint(x: lineLosePointX, y: lineLosePointY))
+        context.addLine(to: CGPoint(x: lineEndPointX, y: lineEndPointY))
+        context.setLineWidth(1.0)
+        context.setFillColor(color.cgColor)
+        context.strokePath()
+        
+        // 段落样式,中心点左边就段落样式就靠左边
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = lineEndPointX < frame.size.width / 2.0 ? .left : .right
+        
+        // 画折线上面的数字+比例
+        let numberFinalX = (numberStartPointX >= frame.size.width / 2) ? numberStartPointX - 5 : numberStartPointX + 1
+        numberData.draw(in: CGRect(origin: CGPoint(x: numberFinalX, y: numberStartPointY), size: CGSize(width: width, height: height)),
+                        withAttributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 10.0), NSForegroundColorAttributeName: color])
+        
+        // 画文本说明
+        let textFinalX = (textStartPointX >= frame.size.width / 2) ? textStartPointX - 1 : textStartPointX + 1
+        textList[index].draw(in: CGRect(origin: CGPoint(x: textFinalX, y: textStartPointY), size: CGSize(width: width, height: height)), withAttributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 12.0), NSForegroundColorAttributeName: color, NSParagraphStyleAttributeName: paragraphStyle])
     }
   
     // Only override draw() if you perform custom drawing.
@@ -47,21 +126,19 @@ class PieChartView: UIView {
         // 偏转的弧度
         var offsetRadian: CGFloat = 0.0
         var startAngle: CGFloat = 0.0
+        // 数值的总和
+        let sum = datas.reduce(0,{$0 + $1})
         for (index,element) in datas.enumerated() {
             // 每个数据对应的角度
-            let radian = element * 360.0
+            let radian = (element / sum) * 360.0
             let endAngle = radian.degreesToRadians() + offsetRadian
             offsetRadian += radian.degreesToRadians()
-            //let startAngle = 0 + currentRadian
-            //let endAngle = startAngle + radian
             context.setFillColor(colors[index].cgColor)
-            
             context.move(to: center)
-            // -90弧度的意思是从12点钟的方位开始画
             context.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
             context.fillPath()
             
-            // MARK: - 画比例直线
+            // MARK: - 画比例线
             drawLine(context, startAngle: startAngle, endAngle: endAngle, color: colors[index],index: index)
             startAngle = endAngle
             
@@ -80,19 +157,16 @@ class PieChartView: UIView {
         
         // MARK: - 画内圈数据
         context.saveGState()
-        //context.setFillColor(UIColor.yellow.cgColor)
-        let text: NSString = "10092.23\n 总计消费"
+        let text: NSString = String.init(format: "%.2f\n 总计消费", sum) as NSString
         let style = NSMutableParagraphStyle()
         style.alignment = .center
         
-        text.draw(in: CGRect(origin: CGPoint(x:center.x-40,y:center.y-20), size: CGSize(width: 80, height: 80)), withAttributes: [
-            NSFontAttributeName: UIFont(name: "HelveticaNeue-Bold", size: 18.0)!,
-            NSForegroundColorAttributeName: UIColor.red,
+        text.draw(in: CGRect(origin: CGPoint(x: center.x - (radius/2), y: center.y - (radius/4)), size: CGSize(width: radius, height: radius)), withAttributes: [
+            NSFontAttributeName: UIFont(name: "HelveticaNeue-Bold", size: 17.0)!,
+            NSForegroundColorAttributeName: UIColor.black,
             NSParagraphStyleAttributeName: style])
         context.restoreGState()
     }
- 
-
 }
 
 
